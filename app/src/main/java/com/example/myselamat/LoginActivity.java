1e1b606dca3762ca4b126f277ff8856fd1e79e9e
jay <group13shop4all@gmail.com>
2021-08-28 16:10:47 +0800

    Low Wei Han Changes/ New File: 1. Changed SplashActivity background design 2. Added Login Activity

---
+++ app/src/main/java/com/example/myselamat/LoginActivity.java
@@ -0,0 +1,123 @@
+package com.example.myselamat;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class LoginActivity extends AppCompatActivity {
+
+    TextView forgot_password, register;
+    EditText email, password;
+    Button login_button;
+    String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
+    ProgressDialog progressDialog;
+
+    FirebaseAuth mAuth;
+    FirebaseUser mUser;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        forgot_password = (TextView) findViewById(R.id.forgot_password);
+        register = (TextView) findViewById(R.id.register);
+        email = (EditText) findViewById(R.id.email);
+        password = (EditText) findViewById(R.id.password);
+        login_button = (Button) findViewById(R.id.login_button);
+
+        progressDialog = new ProgressDialog(this);
+
+        mAuth = FirebaseAuth.getInstance();
+        mUser = mAuth.getCurrentUser();
+
+        forgot_password.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(LoginActivity.this, ForgotPasswordActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        login_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                performLogin();
+            }
+        });
+
+    }
+
+    private void performLogin() {
+
+        String lEmail = email.getText().toString().trim();
+        String lPassword = password.getText().toString().trim();
+
+        if (lEmail.isEmpty()){
+
+            email.setError("Required");
+        }else if (!lEmail.matches(emailPattern)){
+
+            email.setError("Invalid Email");
+        }else if (lPassword.isEmpty()){
+
+            password.setError("Required");
+        }else {
+
+            progressDialog.setTitle("Login");
+            progressDialog.setMessage("Login Processing...");
+            progressDialog.setCanceledOnTouchOutside(false);
+            progressDialog.show();
+
+            mAuth.signInWithEmailAndPassword(lEmail, lPassword).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+
+                    if (task.isSuccessful()){
+
+                        progressDialog.dismiss();
+                        sendUserToMainActivity();
+                        Toast.makeText(LoginActivity.this, "Login Successful", Toast.LENGTH_SHORT).show();
+                    }
+                    else {
+
+                        progressDialog.dismiss();
+                        Toast.makeText(LoginActivity.this, "Error: "+task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        }
+    }
+
+    private void sendUserToMainActivity(){
+
+        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+    }
+}
